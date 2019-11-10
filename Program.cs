92ed950f4a2e106d57944f5eb9c4af54a18d38fb
Igor <bistrovic.igor@gmail.com>
2019-11-10 12:49:13 +0100

    inital add

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fininex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
