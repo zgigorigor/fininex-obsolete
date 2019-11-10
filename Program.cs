1247084c22b4de7ce95f3a00e3a100d60dbcfc49
Igor <bistrovic.igor@gmail.com>
2019-11-10 23:20:50 +0100

    inital add

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+
+namespace fininex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Start fininex? ");
+            var startApp = Console.ReadLine();
+            if (startApp == "no")
+            {
+                Console.WriteLine("Fininex terminated. Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Fininex starting...");
+            }
+
+            Console.Write("Use today's date? (y/n) ");
+            string dateQ = Console.ReadLine();
+            if (dateQ == "n")
+            {
+                Console.WriteLine("Starting fininex history...");
+            }
+            else
+            {
+
+                DateTime now = DateTime.Now;
+                Console.WriteLine(now.ToString("d"));
+            }
+            
+        }
+    }
+}
