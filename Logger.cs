a36225a693388987dd309ed87b06a0ee2b3c0658
Igor <bistrovic.igor@gmail.com>
2019-11-11 00:51:11 +0100

    implemented simple logger

--- Logger.cs
+++ Logger.cs
@@ -6,5 +6,17 @@ namespace fininex
 {
     class Logger
     {
+        public void LoggerControl(String lines)
+        {
+        
+            // Write the string to a file.append mode is enabled so that the log
+            // lines get appended to  test.txt than wiping content and writing the log
+        
+             System.IO.StreamWriter file = new System.IO.StreamWriter("C:\\Igor\\dev\\Training\\fininex\\fininextest.txt", true);
+             file.WriteLine(lines);
+        
+             file.Close();
+        
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace fininex
     {
         static void Main(string[] args)
         {
+            var testlogPath = "C:\\Igor\\dev\\Training\\fininex\\fininextest.txt";
+            //var logPath = "C:\\Igor\\dev\\Training\\fininex\\fininexlog.txt";
             // LOGGER START
+            System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} Fininex booting...\n");
             // APP START
             Console.Write("Start fininex? ");
             var startApp = Console.ReadLine();
