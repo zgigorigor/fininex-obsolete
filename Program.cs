1225540e70f8878984318e130f4a787757e4aee1
Igor <bistrovic.igor@gmail.com>
2019-11-11 21:43:37 +0100

    implemented logger into Project.cs

--- Program.cs
+++ Program.cs
@@ -10,17 +10,20 @@ namespace fininex
             var testlogPath = "C:\\Igor\\dev\\Training\\fininex\\fininextest.txt";
             //var logPath = "C:\\Igor\\dev\\Training\\fininex\\fininexlog.txt";
             // LOGGER START
-            System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} Fininex booting...\n");
+            System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} .. fininex booting...\n");
             // APP START
-            Console.Write("Start fininex? ");
+            Console.Write("Start fininex? (y/n) ");
             var startApp = Console.ReadLine();
-            if (startApp == "no")
+            if (startApp == "n")
             {
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} ?? start fininex user choice: no\n");
                 Console.WriteLine("Fininex terminated. Goodbye!");
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} !! finninex closed.\n");
             }
             else
             {
                 Console.WriteLine("Fininex starting...");
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} !! finninex started.\n");
             }
 
             // DATE CHECK
@@ -28,19 +31,24 @@ namespace fininex
             string dateQ = Console.ReadLine();
             if (dateQ == "n")
             {
-                Console.WriteLine("Starting fininex history...");
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} ?? date user choice: no\n");
+                Console.WriteLine("Starting fininex history mode...");
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} .. start fininex history mode\n");
                 Console.Write("Please enter date: (YY/MM/DD) ");
                 var dateHistory = Console.ReadLine();
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} UI {dateHistory}\n");
                 Console.WriteLine(dateHistory);
             }
             else
             {
-
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} ?? date user choice: yes\n");
                 DateTime now = DateTime.Now;
+                System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} UI {now.ToString("d")}\n");
                 Console.WriteLine(now.ToString("d"));
                                 
             }
-            
+
+            System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} !! fininex terminated.\n");
             // INCOME INPUT
             // EXPENSES INPUT
             // BALANCE CALCULATION
@@ -48,3 +56,6 @@ namespace fininex
         }
     }
 }
+
+
+// System.IO.File.AppendAllText(testlogPath, $"{DateTime.Now} \n");
